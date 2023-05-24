107549b8a65490520aac89741f0bf3521e799b4d
Кулагин Алексей <tcrazywolf63@gmail.com>
2023-05-24 11:33:27 +0400

    FIx

--- LabsPsutiKR/ViewModel/StartWindowViewModel.cs
+++ LabsPsutiKR/ViewModel/StartWindowViewModel.cs
@@ -2,9 +2,11 @@
 using CommunityToolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace LabsPsutiKR.ViewModel
 {
@@ -19,12 +21,12 @@ namespace LabsPsutiKR.ViewModel
         [ObservableProperty] private int selectedTask;
 
         /* X и ИГРЕС*/
-        [ObservableProperty] private List<double> arrayX;
-        [ObservableProperty] private List<double> arrayY;
+        [ObservableProperty] private ObservableCollection<double> arrayX;
+        [ObservableProperty] private ObservableCollection<double> arrayY;
 
         /* Сортировка */
         [ObservableProperty] private string textResult = "Результат пол или отриц чисел";
-        [ObservableProperty] private List<double> sortArray;
+        [ObservableProperty] private ObservableCollection<double> sortArray;
 
         public StartWindowViewModel()
         {
@@ -41,12 +43,11 @@ namespace LabsPsutiKR.ViewModel
             double end = 5.0; // Конечное значение диапазона x
             double step = 0.5; // Шаг
 
-            double A = 0.0; // Значение A
+            double A = SelectedValueA; // Значение A
 
             for (double x = start; x <= end; x += step)
             {
                 double y = SolveEquation(x, A);
-
 
                 if (double.IsNaN(x) || double.IsNaN(y))
                     continue;
@@ -56,7 +57,6 @@ namespace LabsPsutiKR.ViewModel
                
             }
         }
-
         public static double SolveEquation(double x, double A)
         {
             double y;
@@ -74,10 +74,142 @@ namespace LabsPsutiKR.ViewModel
         }
 
 
+        [RelayCommand]
+        private void MaxItemArray()
+        {
+            ObservableCollection<double> templist = ServiceTwoListToOne(ArrayX, ArrayY);
+            MessageBox.Show(templist.Max().ToString());
+        }
+
+        [RelayCommand]
+        private void MinItemArray()
+        {
+            ObservableCollection<double> templist = ServiceTwoListToOne(ArrayX, ArrayY);
+            MessageBox.Show(templist.Min().ToString());
+        }
+
+        [RelayCommand]
+        private void SumItemsArray()
+        {
+            ObservableCollection<double> templist = ServiceTwoListToOne(ArrayX, ArrayY);
+            MessageBox.Show(templist.Sum().ToString());
+        }
+
+        [RelayCommand]
+        private void MultipleItemsArray()
+        {
+            ObservableCollection<double> templist = ServiceTwoListToOne(ArrayX, ArrayY);
+
+            double result = 0;
+
+            foreach (var item in templist)
+            {
+                if (result == 0)
+                    result = item;
+
+                result *= item;
+            }
+            
+            MessageBox.Show(result.ToString());
+        }
 
 
+        [RelayCommand]
+        private void SumNegativeItemsArray()
+        {
+            ObservableCollection<double> templist = ServiceTwoListToOne(ArrayX, ArrayY);
+            MessageBox.Show(templist.Where(x=> x <= 0).Sum().ToString());
+        }
+
+        [RelayCommand]
+        private void SumPositiveItemsArray()
+        {
+            ObservableCollection<double> templist = ServiceTwoListToOne(ArrayX, ArrayY);
+            MessageBox.Show(templist.Where(x => x >= 0).Sum().ToString());
+        }
 
 
+        [RelayCommand]
+        private void MultiplePsotiveItemsArray()
+        {
+            ObservableCollection<double> templist = ServiceTwoListToOne(ArrayX, ArrayY);
+            var s = templist.Where(x => x >= 0);
+            double result = 0;
+
+            foreach (var item in s)
+            {
+                if (result == 0)
+                    result = item;
+                result *= item;
+            }
+
+            MessageBox.Show(result.ToString());
+        }
+
+        [RelayCommand]
+        private void MultipleNegativeItemsArray()
+        {
+            ObservableCollection<double> templist = ServiceTwoListToOne(ArrayX, ArrayY);
+            var s = templist.Where(x => x <= 0);
+            double result = 0;
+
+            foreach (var item in s)
+            {
+                if (result == 0)
+                    result = item;
+                result *= item;
+            }
+
+            MessageBox.Show(result.ToString());
+        }
+
+
+        [RelayCommand]
+        private void SortPositiveArray()
+        {
+            textResult = "Пол элементы массива";
+            var s = ServiceTwoListToOne(ArrayX, ArrayY).Where(x => x >= 0).Order().ToList();
+            SortArray = new();
+            foreach (var item in s)
+            {
+                SortArray.Add(item);
+            }
+        }
+
+        [RelayCommand]
+        private void SortNegativeArray()
+        {
+            textResult = "Пол элементы массива";
+            var s = ServiceTwoListToOne(ArrayX, ArrayY).Where(x => x <= 0).Order().ToList();
+            SortArray = new();
+            foreach (var item in s)
+            {
+                SortArray.Add(item);
+            }
+        }
+
+
+        private ObservableCollection<double> ServiceTwoListToOne(ObservableCollection<double> one, 
+            ObservableCollection<double> two)
+        {
+            ObservableCollection<double> newList = new();
+
+            foreach (var item in one)
+            {
+                if (newList.Contains(item))
+                    continue;
+                newList.Add(item);
+            }
+
+            foreach (var item in two)
+            {
+                if (newList.Contains(item))
+                    continue;
+                newList.Add(item);
+            }
+
+            return newList;
+        }
 
     }
 }
